65f6255bdcc6befba28825d979926510428673db
tennin12 <bassembenyhia12@gmail.com>
2021-07-21 13:14:22 -0400

    remove betty errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,22 +6,22 @@
  * @parent: root of node.
  * @value: value in the node.
  *
- * Return: Always 0 (Success)
+ * Return: node or NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+binary_tree_t *node;
 
-    if (parent == NULL)
-        return (NULL);
-    node = binary_tree_node(parent, value);
-    if (node == NULL)
-        return (NULL);
-    node->left = parent->left;
-    if (node->left != NULL)
-    {
-        node->left->parent = node;
-    }
-    parent->left = node
-    return (node);
+if (parent == NULL)
+return (NULL);
+node = binary_tree_node(parent, value);
+if (node == NULL)
+return (NULL);
+node->left = parent->left;
+if (node->left != NULL)
+{
+node->left->parent = node;
+}
+parent->left = node
+return (node);
 }
