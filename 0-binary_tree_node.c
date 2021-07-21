aa390b8a0f5c2c31bc80e19acb51124568ab407f
tennin12 <bassembenyhia12@gmail.com>
2021-07-21 12:38:49 -0400

    fixed f0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,23 @@
-dgsd
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ *
+ * @parent: root of node.
+ * @value: value in the node.
+ *
+ * Return: Always 0 (Success)
+ */
+ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+ {
+    binary_tree_t *temp;
+
+    temp = malloc(sizeof(binary_tree_t));
+    if (temp == NULL)
+            Return (NULL);
+    temp->parent = parent;
+    temp->n = value;
+    temp->right = NULL;
+    temp->left = NULL;
+    Return (temp);
+ }
